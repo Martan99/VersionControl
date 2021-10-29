95651b928e7bad1e296269e60035db201e4171ac
Martan99 <77725634+Martan99@users.noreply.github.com>
2021-10-29 19:48:27 +0200

    Call webService

--- WebService/Form1.cs
+++ WebService/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WebService.Entities;
+using WebService.MnbServiceReference;
 
 namespace WebService
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void ss()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
