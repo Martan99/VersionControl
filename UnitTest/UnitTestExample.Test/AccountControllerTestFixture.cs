eeb724293119a23a449ba7cb28b511fc9b95432e
Martan99 <77725634+Martan99@users.noreply.github.com>
2021-11-26 23:23:18 +0100

    Add AAA

--- UnitTest/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTest/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,13 +1,27 @@
 ﻿using NUnit.Framework;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test]
+        [
+    Test,
+    TestCase("abcd1234", false),
+    TestCase("irf@uni-corvinus", false),
+    TestCase("irf.uni-corvinus.hu", false),
+    TestCase("irf@uni-corvinus.hu", true)
+]
         public void TestValidateEmail(string email, bool expectedResult)
         {
-            // ...
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
