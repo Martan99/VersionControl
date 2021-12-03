9a9a3973cd72ceb7dbd81d2608adf479eed0e3a5
Martan99 <77725634+Martan99@users.noreply.github.com>
2021-12-03 16:38:06 +0100

    Create entities

---
+++ Microsimulations/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsimulations.Entities
+{
+    class BirthProbability
+    {
+        public int Age { get; set; }
+        public int KidsCount { get; set; }
+        public double P { get; set; }
+
+    }
+}
