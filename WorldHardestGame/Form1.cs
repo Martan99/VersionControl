fc1ab96d6555a90a97aaa05852fe90a633af75ef
Martan99 <77725634+Martan99@users.noreply.github.com>
2021-12-09 19:00:51 +0100

    try

--- WorldHardestGame/Form1.cs
+++ WorldHardestGame/Form1.cs
@@ -22,6 +22,8 @@ namespace WorldHardestGame
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+            gc.AddPlayer();
+            gc.Start(true);
         }
     } 
 }
