836d7203d483b9b90e540ad015fd7acc72cfbb9d
Martan99 <77725634+Martan99@users.noreply.github.com>
2021-12-09 19:13:03 +0100

    The race

--- WorldHardestGame/Form1.cs
+++ WorldHardestGame/Form1.cs
@@ -76,5 +76,14 @@ namespace WorldHardestGame
 
             gc.Start();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
+        }
     }
 }
